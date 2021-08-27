1e564d906d2d97c5ff26e1acf69c0573132e246f
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-08-27 11:03:44 +0300

    TAQ-51 Ölçü aleti/türü model ve ilişkilerinin tanımlanması

--- app/Models/MeasurementToolType.php
+++ app/Models/MeasurementToolType.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MeasurementToolType extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description'
+    ];
 }
