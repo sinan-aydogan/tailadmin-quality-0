f40e09569a39e238c15ade53a81fbe5dca9ad1e7
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-09-13 02:41:19 +0300

    Property ve Property Type seederları oluşturuldu

---
+++ database/seeders/PropertySeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Property;
+use Illuminate\Database\Seeder;
+
+class PropertySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*Package Types*/
+        /*Palette*/
+        Property::create([
+            'name' => 'Palette',
+            'property_type_id' => 1,
+            'creator_id' => 1
+        ]);
+        /*Craft Bag*/
+        Property::create([
+            'name' => 'Craft Bag',
+            'property_type_id' => 1,
+            'creator_id' => 1
+        ]);
+        /*Barrel*/
+        Property::create([
+            'name' => 'Barrel',
+            'property_type_id' => 1,
+            'creator_id' => 1
+        ]);
+
+        /*Storage Conditions*/
+        /*Dry and Indoor*/
+        Property::create([
+            'name' => 'Dry and Indoor',
+            'property_type_id' => 2,
+            'creator_id' => 1
+        ]);
+        /*Outside*/
+        Property::create([
+            'name' => 'Outside',
+            'property_type_id' => 2,
+            'creator_id' => 1
+        ]);
+        /*Far away from Reactive Materials*/
+        Property::create([
+            'name' => 'Far away from Reactive Materials',
+            'property_type_id' => 2,
+            'creator_id' => 1
+        ]);
+        /*Protected from Heat*/
+        Property::create([
+            'name' => 'Protected from Heat',
+            'property_type_id' => 2,
+            'creator_id' => 1
+        ]);
+    }
+}
