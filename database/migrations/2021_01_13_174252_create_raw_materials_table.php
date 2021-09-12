118e20eb42237f0921dff8e089570e63c32994d6
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-09-13 02:39:10 +0300

    supplier_id kaldırıldı

--- database/migrations/2021_01_13_174252_create_raw_materials_table.php
+++ database/migrations/2021_01_13_174252_create_raw_materials_table.php
@@ -20,11 +20,8 @@ class CreateRawMaterialsTable extends Migration
             $table->string('model',255)->nullable();
             $table->string('manufacturer',255)->nullable();
             $table->string('description',750)->nullable();
-            $table->foreignId('supplier_id');
             $table->foreignId('department_id');
             $table->foreignId('raw_material_type_id');
-            $table->json('package_type')->nullable();
-            $table->json('stock_rules')->nullable();
             $table->foreignId('creator_id');
             $table->foreignId('updater_id')->nullable();
             $table->foreignId('deleter_id')->nullable();
