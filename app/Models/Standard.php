5b85d50543057377fa2d4b175cb4f423bd3a9c11
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-09-19 04:19:14 +0300

    Stadartlara çoklu(morph) ilişkilendirme özelliği eklendi

--- app/Models/Standard.php
+++ app/Models/Standard.php
@@ -18,12 +18,56 @@ class Standard extends Model
      *
      * @var array
      */
-    /*protected $fillable = [];*/
+    protected $fillable = [
+        'name',
+        'code',
+        'standard_type',
+        'department_id',
+        'scope',
+        'referred_standards',
+        'status',
+        'creator_id',
+        'updater_id',
+        'deleter_id'
+    ];
 
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    /*protected $casts = [];*/
+    protected $casts = [
+        'referred_standards' => 'array'
+    ];
+
+    //DEPARTMENT
+    /**
+     * Get the department.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function department()
+    {
+        return $this->belongsTo(Department::class)->withDefault(['name' => '']);
+    }
+
+    //RELATED ITEMS WITH STANDARD
+    /**
+     * The related items that belong to the standard.
+     */
+    /**
+     * Get all of the products that are assigned this standard.
+     */
+    public function products()
+    {
+        return $this->morphedByMany(Product::class, 'standardables');
+    }
+
+    /**
+     * Get all of the raw materials that are assigned this standard.
+     */
+    public function rawMaterials()
+    {
+        return $this->morphedByMany(RawMaterial::class, 'standardables');
+    }
 }
