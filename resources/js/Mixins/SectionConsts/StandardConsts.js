05ceaf006d7fe906ffda37cf5225ce4d7e34d53a
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-09-21 04:04:27 +0300

    Standard sabitleri eklendi

---
+++ resources/js/Mixins/SectionConsts/StandardConsts.js
@@ -0,0 +1,22 @@
+export const StandardConsts = {
+    data(){
+        return{
+            standardTypes:[
+                {id:1,name:'Product Standard'},
+                {id:2,name:'Raw Material Standard'},
+                {id:3,name:'Process Standard'},
+                {id:4,name:'Equipment Standard'},
+                {id:5,name:'Analysis Standard'},
+                {id:6,name:'System Standard'},
+                {id:7,name:'Working Area/Condition Standard'},
+                {id:8,name:'Calibration Standard'},
+                {id:9,name:'Other'}
+            ],
+            status : [
+                {id: 1, name: 'Active', color: 'solid-green'},
+                {id: 2, name: 'Passive', color: 'solid-yellow'},
+                {id: 3, name: 'Canceled', color: 'solid-red'}
+            ],
+        }
+    }
+}
