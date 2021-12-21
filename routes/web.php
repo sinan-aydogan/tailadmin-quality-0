f75486f26ed115ae48cf12e0e8c34d47ed925d83
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2021-12-21 04:40:53 +0300

    Regulate route three - Merges The resource routes in array - Completes trash routes - New search index routes (department, staff and job description)

--- routes/web.php
+++ routes/web.php
@@ -18,127 +18,157 @@ use Spatie\Permission\Models\Role;
 */
 
 
-
-Route::middleware(['auth:sanctum', 'verified'])->group(function (){
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/', function () {
-        return Inertia::render('Dashboard',[
+        return Inertia::render('Dashboard', [
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
             'laravelVersion' => Application::VERSION,
             'phpVersion' => PHP_VERSION,
         ]);
-})->name('/');
+    })->name('/');
 
-    /*They are the required pages for the system, don't delete it*/
+    /*Settings Route*/
     Route::prefix('settings')->group(function () {
-       Route::get('/', function () {return Inertia::render('Settings/Index',[
-           'users_count' => count(\App\Models\User::all('id')),
-           'roles_count' => count(Role::all()),
-           'permissions_count' => count(Permission::all())
-       ]);})->name('settings');
-       Route::resource('settings-user', \App\Http\Controllers\Settings\UserController::class);
-       Route::get('role', function () {return Inertia::render('Settings/Role');})->name('settings-role');
-       Route::get('permission', function () {return Inertia::render('Settings/Permission');})->name('settings-permission');
-       Route::get('system', function () {return Inertia::render('Settings/System');})->name('settings-system');
+        Route::get('/', function () {
+            return Inertia::render('Settings/Index', [
+                'users_count' => count(\App\Models\User::all('id')),
+                'roles_count' => count(Role::all()),
+                'permissions_count' => count(Permission::all())
+            ]);
+        })->name('settings');
+        Route::resource('settings-user', \App\Http\Controllers\Settings\UserController::class);
+        Route::get('role', function () {
+            return Inertia::render('Settings/Role');
+        })->name('settings-role');
+        Route::get('permission', function () {
+            return Inertia::render('Settings/Permission');
+        })->name('settings-permission');
+        Route::get('system', function () {
+            return Inertia::render('Settings/System');
+        })->name('settings-system');
         /*Property Management*/
         Route::resource('settings-property', \App\Http\Controllers\Settings\PropertyController::class);
         Route::resource('settings-property-type', \App\Http\Controllers\Settings\PropertyTypeController::class);
     });
 
-    /*Tailadmin Quality Routes*/
+
+    /*LaraQuality Resource Routes*/
+    Route::resources([
+        'department' => \App\Http\Controllers\DepartmentController::class,
+        'staff' => \App\Http\Controllers\StaffController::class,
+        'job-description' => \App\Http\Controllers\JobDescriptionController::class,
+        'machine' => \App\Http\Controllers\MachineController::class,
+        'machine-type' => \App\Http\Controllers\MachineTypeController::class,
+        'maintenance' => \App\Http\Controllers\MaintenanceController::class,
+        'maintenance-action' => \App\Http\Controllers\MaintenanceActionController::class,
+        'maintenance-plan' => \App\Http\Controllers\MaintenancePlanController::class,
+        'maintenance-type' => \App\Http\Controllers\MaintenanceTypeController::class,
+        'product' => \App\Http\Controllers\ProductController::class,
+        'product-type' => \App\Http\Controllers\ProductTypeController::class,
+        'product-quality-plan' => \App\Http\Controllers\ProductQualityPlanController::class,
+        'recipe' => \App\Http\Controllers\RecipeController::class,
+        'raw-material' => \App\Http\Controllers\RawMaterialController::class,
+        'raw-material-type' => \App\Http\Controllers\RawMaterialTypeController::class,
+        'raw-material-quality-plan' => \App\Http\Controllers\RawMaterialQaulityPlanController::class,
+        'quality-spect' => \App\Http\Controllers\QualitySpectController::class,
+        'supplier' => \App\Http\Controllers\SupplierController::class,
+        'audit' => \App\Http\Controllers\AuditController::class,
+        'audit-type' => \App\Http\Controllers\AuditTypeController::class,
+        'audit-plan' => \App\Http\Controllers\AuditPlanController::class,
+        'audit-firm' => \App\Http\Controllers\AuditFirmController::class,
+        'auditor' => \App\Http\Controllers\AuditorController::class,
+        'standard' => \App\Http\Controllers\StandardController::class,
+        'document' => \App\Http\Controllers\DocumentTypeController::class,
+        'document-type' => \App\Http\Controllers\DocumentTypeController::class,
+        'complaint' => \App\Http\Controllers\ComplaintController::class,
+        'complaint-type' => \App\Http\Controllers\ComplaintTypeController::class,
+        'complaint-action' => \App\Http\Controllers\ComplaintActionController::class,
+        'complaint-decision' => \App\Http\Controllers\ComplaintDecisionController::class,
+        'complaint-property' => \App\Http\Controllers\ComplaintPropertyController::class,
+        'customer' => \App\Http\Controllers\CustomerController::class,
+        'measurement-tool' => \App\Http\Controllers\MeasurementToolController::class,
+        'measurement-tool-type' => \App\Http\Controllers\MeasurementToolTypeController::class,
+        'measurement-tool-action' => \App\Http\Controllers\MeasurementToolActionController::class,
+        'measurement-tool-property' => \App\Http\Controllers\MeasurementToolPropertyController::class
+    ]);
+
+    /*Custom Routes: Search Index, Deleted Items*/
     //Department
-    Route::resource('department', \App\Http\Controllers\DepartmentController::class);
+    Route::post('department', [\App\Http\Controllers\DepartmentController::class, 'index'])->name('department.index');
     Route::get('department/trash', [\App\Http\Controllers\DepartmentController::class, 'trash'])->name('department.trash');
     //Staff
-    Route::resource('staff', \App\Http\Controllers\StaffController::class);
+    Route::post('staff', [\App\Http\Controllers\StaffController::class, 'index'])->name('staff.index');
     Route::get('staff/trash', [\App\Http\Controllers\StaffController::class, 'trash'])->name('staff.trash');
     //Job Description
-    Route::resource('job-description', \App\Http\Controllers\JobDescriptionController::class);
+    Route::post('job-description', [\App\Http\Controllers\JobDescriptionController::class, 'index'])->name('job-description.index');
     Route::get('job-description/trash', [\App\Http\Controllers\JobDescriptionController::class, 'trash'])->name('job-description.trash');
     //Machine
-    Route::resource('machine', \App\Http\Controllers\MachineController::class);
     Route::get('machine/trash', [\App\Http\Controllers\MachineController::class, 'trash'])->name('machine.trash');
     //Machine Type
-    Route::resource('machine-type', \App\Http\Controllers\MachineTypeController::class);
-    Route::get('machine-type/trash', [\App\Http\Controllers\MachineTypeController::class, 'trash'])
-        ->name('machine-type.trash');
+    Route::get('machine-type/trash', [\App\Http\Controllers\MachineTypeController::class, 'trash'])->name('machine-type.trash');
     //Maintenance
-    Route::resource('maintenance', \App\Http\Controllers\MaintenanceController::class);
+    Route::get('maintenance/trash', [\App\Http\Controllers\MaintenanceController::class, 'trash'])->name('maintenance.trash');
     //Maintenance Action
-    Route::resource('maintenance-action', \App\Http\Controllers\MaintenanceActionController::class);
+    Route::get('maintenance-action/trash', [\App\Http\Controllers\MaintenanceActionController::class, 'trash'])->name('maintenance-action.trash');
     //Maintenance Plan
-    Route::resource('maintenance-plan', \App\Http\Controllers\MaintenancePlanController::class);
+    Route::get('maintenance-plan/trash', [\App\Http\Controllers\MaintenancePlanController::class, 'trash'])->name('maintenance-plan.trash');
     //Maintenance Type
-    Route::resource('maintenance-type', \App\Http\Controllers\MaintenanceTypeController::class);
+    Route::get('maintenance-type/trash', [\App\Http\Controllers\MaintenanceTypeController::class, 'trash'])->name('maintenance-type.trash');
     //Product
-    Route::resource('product', \App\Http\Controllers\ProductController::class);
     Route::get('product/trash', [\App\Http\Controllers\ProductController::class, 'trash'])->name('product.trash');
     //Product Type
-    Route::resource('product-type', \App\Http\Controllers\ProductTypeController::class);
     Route::get('product-type/trash', [\App\Http\Controllers\ProductTypeController::class, 'trash'])->name('product-type.trash');
     //Product Quality Plan
-    Route::resource('product-quality-plan', \App\Http\Controllers\ProductQualityPlanController::class);
     Route::get('product-quality-plan/trash', [\App\Http\Controllers\ProductQualityPlanController::class, 'trash'])->name('product-quality-plan.trash');
     //Recipe
-    Route::resource('recipe', \App\Http\Controllers\RecipeController::class);
     Route::get('recipe/trash', [\App\Http\Controllers\RecipeController::class, 'trash'])->name('recipe.trash');
     //Raw Material
-    Route::resource('raw-material', \App\Http\Controllers\RawMaterialController::class);
     Route::get('raw-material/trash', [\App\Http\Controllers\RawMaterialController::class, 'trash'])->name('raw-material.trash');
     //Raw Material Type
-    Route::resource('raw-material-type', \App\Http\Controllers\RawMaterialTypeController::class);
     Route::get('raw-material-type/trash', [\App\Http\Controllers\RawMaterialController::class, 'trash'])->name('raw-material-type.trash');
     //Raw Material Quality Plan
-    Route::resource('raw-material-quality-plan', \App\Http\Controllers\RawMaterialQaulityPlanController::class);
     Route::get('raw-material-quality-plan/trash', [\App\Http\Controllers\RawMaterialQaulityPlanController::class, 'trash'])->name('raw-material-quality-plan.trash');
     //Quality Spect
-    Route::resource('quality-spect', \App\Http\Controllers\QualitySpectController::class);
     Route::get('quality-spect/trash', [\App\Http\Controllers\QualitySpectController::class, 'trash'])->name('quality-spect.trash');
     //Suppliers
-    Route::resource('supplier', \App\Http\Controllers\SupplierController::class);
+    Route::get('supplier/trash', [\App\Http\Controllers\SupplierController::class, 'trash'])->name('supplier.trash');
     //Audit
-    Route::resource('audit', \App\Http\Controllers\AuditController::class);
+    Route::get('audit/trash', [\App\Http\Controllers\AuditController::class, 'trash'])->name('audit.trash');
     //Audit Type
-    Route::resource('audit-type', \App\Http\Controllers\AuditTypeController::class);
     Route::get('audit-type/trash', [\App\Http\Controllers\AuditTypeController::class, 'trash'])->name('audit-type.trash');
     //Audit Plan
-    Route::resource('audit-plan', \App\Http\Controllers\AuditPlanController::class);
+    Route::get('audit-plan/trash', [\App\Http\Controllers\AuditPlanController::class, 'trash'])->name('audit-plan.trash');
     //Audit Firm
-    Route::resource('audit-firm', \App\Http\Controllers\AuditFirmController::class);
     Route::get('audit-firm/trash', [\App\Http\Controllers\AuditFirmController::class, 'trash'])->name('audit-firm.trash');
     //Auditor
-    Route::resource('auditor', \App\Http\Controllers\AuditorController::class);
     Route::get('auditor/trash', [\App\Http\Controllers\AuditorController::class, 'trash'])->name('auditor.trash');
     //Standard
-    Route::resource('standard', \App\Http\Controllers\StandardController::class);
     Route::get('standard/trash', [\App\Http\Controllers\StandardController::class, 'trash'])->name('standard.trash');
     //Document
-    Route::resource('document', \App\Http\Controllers\DocumentTypeController::class);
+    Route::get('document/trash', [\App\Http\Controllers\DocumentController::class, 'trash'])->name('document.trash');
     //Document Type
-    Route::resource('document-type', \App\Http\Controllers\DocumentTypeController::class);
+    Route::get('document-type/trash', [\App\Http\Controllers\DocumentTypeController::class, 'trash'])->name('document-type.trash');
     //Claim
-    Route::resource('complaint', \App\Http\Controllers\ComplaintController::class);
+    Route::get('complaint/trash', [\App\Http\Controllers\ComplaintController::class, 'trash'])->name('complaint.trash');
     //Claim Type
-    Route::resource('complaint-type', \App\Http\Controllers\ComplaintTypeController::class);
+    Route::get('complaint-type/trash', [\App\Http\Controllers\ComplaintTypeController::class, 'trash'])->name('complaint-type.trash');
     //Claim Action
-    Route::resource('complaint-action', \App\Http\Controllers\ComplaintActionController::class);
+    Route::get('complaint-action/trash', [\App\Http\Controllers\ComplaintActionController::class, 'trash'])->name('complaint-action.trash');
     //Claim Decision
-    Route::resource('complaint-decision', \App\Http\Controllers\ComplaintDecisionController::class);
+    Route::get('complaint-decision/trash', [\App\Http\Controllers\ComplaintDecisionController::class, 'trash'])->name('complaint-decision.trash');
     //Claim Property
-    Route::resource('complaint-property', \App\Http\Controllers\ComplaintPropertyController::class);
+    Route::get('complaint-property/trash', [\App\Http\Controllers\ComplaintPropertyController::class, 'trash'])->name('complaint-property.trash');
     //Customer
-    Route::resource('customer', \App\Http\Controllers\CustomerController::class);
     Route::get('customer/trash', [\App\Http\Controllers\CustomerController::class, 'trash'])->name('customer.trash');
     //Measurement Tool
-    Route::resource('measurement-tool', \App\Http\Controllers\MeasurementToolController::class);
     Route::get('measurement-tool/trash', [\App\Http\Controllers\MeasurementToolController::class, 'trash'])->name('measurement-tool.trash');
     //Measurement Tool Type
-    Route::resource('measurement-tool-type', \App\Http\Controllers\MeasurementToolTypeController::class);
     Route::get('measurement-tool-type/trash', [\App\Http\Controllers\MeasurementToolTypeController::class, 'trash'])->name('measurement-tool-type.trash');
     //Measurement Tool Action
-    Route::resource('measurement-tool-action', \App\Http\Controllers\MeasurementToolActionController::class);
+    Route::get('measurement-tool-action/trash', [\App\Http\Controllers\MeasurementToolActionController::class, 'trash'])->name('measurement-tool-action.trash');
     //Measurement Tool Property
-    Route::resource('measurement-tool-property', \App\Http\Controllers\MeasurementToolPropertyController::class);
+    Route::get('measurement-tool-property/trash', [\App\Http\Controllers\MeasurementToolPropertyController::class, 'trash'])->name('measurement-tool-property.trash');
 
 });
 
