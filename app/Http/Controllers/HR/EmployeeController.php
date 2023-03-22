4ac4f6c7ab992adab803e822d7a465924ac7fd54
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2023-03-22 23:03:22 +0300

    Adds Human Resources models, migrations and controller skeleation

---
+++ app/Http/Controllers/HR/EmployeeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\HR;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/HR/LeaveTypeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\HR;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LeaveTypeController extends Controller
+{
+    //
+}
